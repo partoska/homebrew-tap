07097f58564e04eeeb1bc20c03d6462a0ba34d78
p6a-ops <github@partoska.com>
2026-06-18 17:07:11 +0200

    Add Linux amd64 and arm64 variants to p6a formula

--- Formula/p6a.rb
+++ Formula/p6a.rb
@@ -9,8 +9,25 @@ class P6a < Formula
     sha256 "f8ec83d23987f8bbdd1baeb674af79df550ce939e789f233b4fea3000185bf76"
   end
 
+  on_linux do
+    on_intel do
+      url "https://github.com/partoska/p6a-cmd/releases/download/v#{version}/p6a_#{version}_linux_amd64"
+      sha256 "b96ba999fce92a5c8ffadb84df2de5c9e25f689fa60b20f8d41781e5b2ee7ef5"
+    end
+    on_arm do
+      url "https://github.com/partoska/p6a-cmd/releases/download/v#{version}/p6a_#{version}_linux_arm64"
+      sha256 "322344d25ba28bc08ccf65a5989aa4665e4186924183b578fc4f314f0a5f2c25"
+    end
+  end
+
   def install
-    bin.install "p6a_#{version}_darwin_universal" => "p6a"
+    if OS.mac?
+      bin.install "p6a_#{version}_darwin_universal" => "p6a"
+    elsif Hardware::CPU.intel?
+      bin.install "p6a_#{version}_linux_amd64" => "p6a"
+    else
+      bin.install "p6a_#{version}_linux_arm64" => "p6a"
+    end
   end
 
   test do
