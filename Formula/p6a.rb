082a3ff49a9db5744102429dc5f96b6f768f3ced
p6a-ops <p6a-ops@users.noreply.github.com>
2026-06-25 14:13:59 +0000

    Update p6a to v1.11.1

--- Formula/p6a.rb
+++ Formula/p6a.rb
@@ -2,21 +2,21 @@ class P6a < Formula
   desc "Command-line tool to manage and sync your event photos from Partoska"
   homepage "https://www.partoska.com/p6a"
   license "MIT"
-  version "1.11.0"
+  version "1.11.1"
 
   on_macos do
     url "https://github.com/partoska/p6a-cmd/releases/download/v#{version}/p6a_#{version}_darwin_universal"
-    sha256 "a141c565fc78f6513bea93a0424bb1a1760f8f6039bbffe27fa48e3c53584486"
+    sha256 "00ec65e137180a683fbd8af4ef54db1d6af08f00923350c2cf027755df2d201f"
   end
 
   on_linux do
     on_intel do
       url "https://github.com/partoska/p6a-cmd/releases/download/v#{version}/p6a_#{version}_linux_amd64"
-      sha256 "dac0baec3da7509fd065d8679d544d39931a68b86b2a5ca61a710397e0dd0fa3"
+      sha256 "30bd88825cf645d6c3b2384a0811090fb7cc9d681f721739deb3e9988e198de7"
     end
     on_arm do
       url "https://github.com/partoska/p6a-cmd/releases/download/v#{version}/p6a_#{version}_linux_arm64"
-      sha256 "1049d5ffae70faace36ffb2ffe8148ffccdbe5c2784fc81b547945e657fb4671"
+      sha256 "d4bcfbcfd26b71662ed0520f082f5f20daced5bfee22c0e2468809ad6e3658b8"
     end
   end
 
